6aeab2842c6a24c60cef3473a916f9bd623a556b
Roberto Riera <cantimploura@gmail.com>
2016-10-24 15:52:44 +0100

    OpenCV added

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
 import ChictopiaDownloader
 import time
+import cv2
 
+print(cv2.__version__)
 
+image = cv2.imread("Photos/bag/1174677.jpg")
+
+cv2.imshow("Bag", image)
+cv2.waitKey(0)
+
+"""
 def tiempo():
     return int(round(time.time() * 1000))
 
@@ -10,3 +18,4 @@ ChictopiaDownloader.ChictopiaDownloader(1, 2)
 tiempo_final = tiempo()
 
 print(tiempo_final-tiempo_empezar)
+"""
